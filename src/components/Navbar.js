a2c9ae417312a6f9d5f494bdf0cae8cc80903e0d
Tobias Legenden <tobiaslegenden@gmail.com>
2024-07-01 22:50:34 +0200

    init again

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,31 +19,36 @@ class Navbar extends Component {
 
     render() {
         const { inventoryVisible } = this.state;
-        const { htmData } = this.props;
+        const { inventory, htmData } = this.props;
 
         return (
-        <div className="navbar">
-            <div className="navbar-section left">
-                <h1>Ascot SiteMinder Inventory Manager</h1>
-            </div>
-            <div className="navbar-section middle">
-                <input type="file" multiple onChange={this.handleFileUpload} />
-            </div>
-            <div className="navbar-section right">
-                <button className="inventory-button" onClick={this.handleInventoryClick}>
-                    Inventory
-                </button>
-                {inventoryVisible && (
-                    <div className="dropdown">
-                    {htmData.map((data, index) => (
-                        <div key={index} className="dropdown-item">
-                            {data.VType}: {data.Dato} {data.År} - Kapacitet: {data.Kapacitet}, Reserveret: {data.Reserveret}, Allotment: {data.Allotment}, Ledige: {data.Ledige}, Belægn.%: {data.BelægnProcent}, Led. u. Al.: {data.LedUAl}, Belægn. % u. Allot.: {data.BelægnProcentUAl}
+            <div className="navbar">
+                <div className="navbar-section left">
+                    <h1>Ascot SiteMinder Inventory Manager</h1>
+                </div>
+                <div className="navbar-section middle">
+                    <input type="file" multiple onChange={this.handleFileUpload} />
+                </div>
+                <div className="navbar-section right">
+                    <button className="inventory-button" onClick={this.handleInventoryClick}>
+                        Inventory
+                    </button>
+                    {inventoryVisible && (
+                        <div className="dropdown">
+                            {Object.entries(inventory).map(([key, value]) => (
+                                <div key={key} className="dropdown-item">
+                                    {key}: {value}
+                                </div>
+                            ))}
+                            {htmData.map((data, index) => (
+                                <div key={index} className="dropdown-item">
+                                    {data.VType}: {data.Dato} {data.År} - Kapacitet: {data.Kapacitet}, Reserveret: {data.Reserveret}, Allotment: {data.Allotment}, Ledige: {data.Ledige}, Belægn.%: {data.BelægnProcent}, Led. u. Al.: {data.LedUAl}, Belægn. % u. Allot.: {data.BelægnProcentUAl}
+                                </div>
+                            ))}
                         </div>
-                    ))}
-                    </div>
-                )}
+                    )}
+                </div>
             </div>
-        </div>
         );
     }
 }
