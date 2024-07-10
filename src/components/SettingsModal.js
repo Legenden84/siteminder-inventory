6f7d60185d7bc1da80cb8f473e19847af457e68a
Tobias Legenden <tobiaslegenden@gmail.com>
2024-07-10 21:24:54 +0200

    modal init

--- src/components/SettingsModal.js
+++ src/components/SettingsModal.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './SettingsModal.css';
 
-const SettingsModal = ({ isOpen, onClose, children }) => {
+const SettingsModal = ({ isOpen, onClose, title, children }) => {
     if (!isOpen) return null;
 
     return (
         <div className="settings-modal-overlay">
             <div className="settings-modal-content">
-                <button className="close-button" onClick={onClose}>Close</button>
-                {children}
+                <div className="settings-modal-header">
+                    <h2>{title}</h2>
+                    <button className="button close-button" onClick={onClose}>
+                        <i className="fa-solid fa-xmark"></i>
+                    </button>
+                </div>
+                <div className="settings-modal-body">
+                    {children}
+                </div>
             </div>
         </div>
     );
